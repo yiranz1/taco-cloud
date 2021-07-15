392677743555ee8153baae12723e409dc066ef64
yrzhou <yrzhou01@gmail.com>
2021-07-15 11:16:31 -0400

    Taco Cloud: Add validation on Controllers

--- src/main/java/com/yiran/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/yiran/tacocloud/controllers/DesignTacoController.java
@@ -6,10 +6,13 @@ import com.yiran.tacocloud.models.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,8 +44,19 @@ public class DesignTacoController {
         return "design";
     }
 
+    /**
+     * Handle POST request to "/design"
+     * @Valid: tells Spring MVC to validate the Taco object
+     *         校验时机： 绑定完表单数据之后，调用processDesign()之前
+     * @param design: submitted by the form
+     * @param errors: validation errors
+     * @return redirect to /orders/current if no validation error occurs, otherwise display the error
+     */
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         // Save taco design, will be done in chapter 3
         log.info("Processing design: " + design);
 
