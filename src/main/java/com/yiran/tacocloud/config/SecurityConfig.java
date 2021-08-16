fdb8bea57a3cf79c1aa87a8967476a5f177d3988
yrzhou <yrzhou01@gmail.com>
2021-08-16 11:18:44 -0400

    Taco Cloud: Use LDAP to store user info

--- src/main/java/com/yiran/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/yiran/tacocloud/config/SecurityConfig.java
@@ -1,10 +1,25 @@
 package com.yiran.tacocloud.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchBase("(uid={0})")
+                .groupSearchBase("ou=group")
+                .groupSearchFilter("member={0}")
+                .passwordCompare()
+                // 指定与给定密码进行比对的是passcode属性，指定密码转码器为bcrypt加密
+                .passwordAttribute("passcode")
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
